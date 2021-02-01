96b3511115d63a962d386d43d183938ce6fda426
Irfan Rifai <m.irfanrifai@gmail.com>
2021-02-01 09:38:58 +0700

    updated: merchant dev

--- config/faspay.php
+++ config/faspay.php
@@ -15,7 +15,7 @@ if (env('FASPAY_IS_PRODUCTION') == true) {
 
     return [
         'endpoint'               => env('FASPAY_IS_PRODUCTION'),
-        'merchant'               => env('FASPAY_MERCHANT'),
+        'merchant'               => env('FASPAY_MERCHANT_DEV'),
         'merchantId'             => env('FASPAY_MERCHANT_ID_DEV'),
         'merchantPassword'       => env('FASPAY_MERCHANT_PASSWORD_DEV'),
         'merchantIdCredit'       => env('FASPAY_MERCHANT_ID_CREDIT_DEV'),
