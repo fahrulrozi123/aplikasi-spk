b41c1beb192c303a607e174282fb513fb1a9cd54
fahrulrozi123 <fahrulrozi99321@gmail.com>
2022-12-17 14:29:06 +0700

    Update: Button detail and delete at menu mahasiswa

--- resources/views/admin/mahasiswa/action-button.blade.php
+++ resources/views/admin/mahasiswa/action-button.blade.php
@@ -1,5 +1,5 @@
 <div class="button-list" data-id="{{$id}}" data-table-target="table-mahasiswa">
-    <button type="button" class="btn btn-icon waves-effect waves-light btn-primary" data-toggle="modal" data-target="#detail" data-id="{{$id}}" onclick="Detail(this);"><i class="fa fa-eye"></i></button>
+    <button type="button" class="btn btn-icon waves-effect waves-light btn-outline-primary" data-toggle="modal" data-target="#detail" data-id="{{$id}}" onclick="Detail(this);"><i class="fa fa-eye"></i></button>
     {{-- <button type="button" class="btn btn-icon waves-effect waves-light btn-warning" data-toggle="modal" data-target="#edit-mahasiswa" onclick="return editDataUser(this);" ><i class="fa fa-edit"></i></button> --}}
-    <button data-url="{{route('admin.mahasiswa.delete')}}" class="btn btn-icon waves-effect waves-light btn-danger" onclick="hapusData(this);"> <i class="fa fa-trash"></i> </button>
+    <button data-url="{{route('admin.mahasiswa.delete')}}" class="btn btn-icon waves-effect waves-light btn-outline-danger" onclick="hapusData(this);"> <i class="fa fa-trash"></i> </button>
 </div>
