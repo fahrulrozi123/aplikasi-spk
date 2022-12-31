eeb89c8565b1cc07f153a22eaec5edb7d6f30e0a
fahrulrozi123 <fahrulrozi99321@gmail.com>
2022-12-31 16:34:00 +0700

    Update template pdf

--- resources/views/export/hasilrekomendasi_pdf.blade.php
+++ resources/views/export/hasilrekomendasi_pdf.blade.php
@@ -5,61 +5,60 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Perhitungan metode topsis</title>
-</head>
-
-<style>
-  table {
-    /* margin: auto; */
-    font-family: "Arial";
-    font-size: 15px;
-  }
-  .table {
-      border-collapse: collapse;
+  <style>
+    table {
+      /* margin: auto; */
+      font-family: "Arial";
       font-size: 15px;
-  }
-  .table th, 
-  .table td {
-      border-bottom: 1px solid #f1e7d8;
-      border-left: 1px solid #f1e7d8;
-      padding: 9px 21px;
-  }
-  .table th, 
-  .table td:last-child {
-      border-right: 1px solid #f1e7d8;
-  }
-  .table td:first-child {
-      border-top: 1px solid #f1e7d8;
-  }
-  caption {
-      caption-side: top;
-      margin-bottom: 10px;
-      font-size: 16px;
-  }
-  
-  /* Table Header */
-  .table thead th {
-      background-color: #b68440;
-      color: #FFFFFF;
-  }
-  
-  /* Table Body */
-  .table tbody td {
-      color: #353535;
-  }
-  .table tbody tr:nth-child(odd) td {
-      background-color: #f1e7d8;
-  }
-  .table tbody tr:hover th,
-  .table tbody tr:hover td {
-      background-color: #f1e7d8;
-      transition: all .2s;
-  }
-  
-  /*Tabel Responsive 1*/
-  /* .table-container {
-      overflow: auto;
-  } */
-</style>
+    }
+    .table {
+        border-collapse: collapse;
+        font-size: 15px;
+    }
+    .table th, 
+    .table td {
+        border-bottom: 1px solid #f1e7d8;
+        border-left: 1px solid #f1e7d8;
+        padding: 9px 21px;
+    }
+    .table th, 
+    .table td:last-child {
+        border-right: 1px solid #f1e7d8;
+    }
+    .table td:first-child {
+        border-top: 1px solid #f1e7d8;
+    }
+    caption {
+        caption-side: top;
+        margin-bottom: 10px;
+        font-size: 16px;
+    }
+    
+    /* Table Header */
+    .table thead th {
+        background-color: #b68440;
+        color: #FFFFFF;
+    }
+    
+    /* Table Body */
+    .table tbody td {
+        color: #353535;
+    }
+    .table tbody tr:nth-child(odd) td {
+        background-color: #f1e7d8;
+    }
+    .table tbody tr:hover th,
+    .table tbody tr:hover td {
+        background-color: #f1e7d8;
+        transition: all .2s;
+    }
+    
+    /*Tabel Responsive 1*/
+    .table-container {
+        overflow: auto;
+    }
+  </style>
+</head>
 
 <h2 style="text-align: center;"><u> Pencarian mahasiswa berprestasi dengan metode topsis </u></h2>
 <body>
